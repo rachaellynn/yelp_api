87fa8ccb1385f5e0432a593054956d1ec0998b9f
rachaellynn <rachael@wordaroundtown.com>
2016-01-13 10:41:47 -0500

    initial commit

--- yelp_api.rb
+++ yelp_api.rb
@@ -5,17 +5,17 @@ require 'json'
 
 #create and configure yelp client with API keys
 Yelp.client.configure do |config| #enter yelp developer credentials here
-  config.consumer_key = "4Z_xEruw7AhaknI9UJ8QQA" 
-  config.consumer_secret = "dlcjh9S01a9uYB4XJCoxm6lL8m0"
-  config.token = "zD8_hS4gG_MvN6TehqhK3UA7wPIQ9mn3"
-  config.token_secret = "k0QlLAiZGPfWdsTFhHaYk7XNyMg"
+  config.consumer_key = "" 
+  config.consumer_secret = ""
+  config.token = ""
+  config.token_secret = ""
 end
 
 CSV.open("YelpHVAC.csv", "wb") do |csv| #creates and names the csv files
 	csv <<["name","url","phone","rating","review_cont","address","city","state","postal_code"] #adds column headers
 	        
 	#make a request to the search API
-	response = Yelp.client.search('Boston', { category_filter: 'hvac' }) #selects all hvac companies
+	response = Yelp.client.search('Boston', { category_filter: 'hvac' }) #selects all hvac companies in Boston
 	limit = 15 #enter a limit here -- the maximum is 2000 per day
 	(0...limit).each do |i|
 		busname = response.businesses[i].name
@@ -30,7 +30,6 @@ CSV.open("YelpHVAC.csv", "wb") do |csv| #creates and names the csv files
 		csv << [busname, busurl, busphone, busrating, busreviewcount, busaddress, buscity, busstate, buspostal]
 		sleep (0.5)
 	end
-
 end
 
 
